0cce92db3bff2508927a31434e6a4d0c8dd98858
acti noide <das0schaf@gmx.de>
2022-11-21 11:25:32 +0100

    aded functionality for calculating with enchantments fixed type issue in item.ts

--- src/TS/calculateNeededRessources.ts
+++ src/TS/calculateNeededRessources.ts
@@ -6,19 +6,28 @@ import { craftingrequirement } from "./structs/craftingrequirement"
 import { craftResource } from "./structs/craftResource"
 
 export let calculateNeededRessources = (enchantmentequiv: number, completeItem: item, TranslationFiles: any) => {
-  let neededRessources: translatedRessource[] = []
+  let neededRessources: translatedRessource[][] = []
   if (!completeItem?.enchantments || enchantmentequiv == 0 || completeItem?.enchantments.enchantment.length == 0) {
     //calculate without enchantments
     calculateCraftingRequierments(TranslationFiles, completeItem.craftingrequirements, neededRessources)
   }
   else {
     //calculate with enchantments
-    throw Error("not implementet yet");
+    let enchantment = completeItem.enchantments.enchantment.find((value) => {
+      if (value.enchantmentlevel == enchantmentequiv) return true
+      else return false
+    })
+    if (!enchantment) {
+      throw Error("enchantment not found")
+    }
+    else {
+      calculateCraftingRequierments(TranslationFiles, enchantment.craftingrequirements, neededRessources)
+    }
   }
   return neededRessources
 }
 
-let calculateCraftingRequierments = (TranslationFiles: any, craftingrequirements: craftingrequirement[] | craftingrequirement, neededRessources: translatedRessource[]) => {
+let calculateCraftingRequierments = (TranslationFiles: any, craftingrequirements: craftingrequirement[] | craftingrequirement, neededRessources: translatedRessource[][]) => {
   if (!Array.isArray(craftingrequirements)) {
     //calculate with one crafting option  
     calculateCraftingRessources(TranslationFiles, craftingrequirements.craftresource, neededRessources)
@@ -31,26 +40,42 @@ let calculateCraftingRequierments = (TranslationFiles: any, craftingrequirements
   }
 }
 
-let calculateCraftingRessources = (TranslationFiles: any, craftresource: craftResource | craftResource[], neededRessources: translatedRessource[]) => {
+let calculateCraftingRessources = (TranslationFiles: any, craftresource: craftResource | craftResource[], neededRessources: translatedRessource[][]) => {
   if (!Array.isArray(craftresource)) {
     //calculate with one ressource
     writeNeededRessources(TranslationFiles, { uniquename: craftresource.uniquename, count: craftresource.count }, neededRessources)
   }
   else {
     //calculate with multiple ressources
+    let ressources: { uniquename: string, count: number }[] = []
     craftresource.forEach(element => {
-      writeNeededRessources(TranslationFiles, { uniquename: element.uniquename, count: element.count }, neededRessources)
-    });
+      ressources.push({ uniquename: element.uniquename, count: element.count })
+    })
+    writeNeededRessources(TranslationFiles, ressources, neededRessources)
   }
 }
 
-let writeNeededRessources = (TranslationFiles: any, element: { uniquename: string, count: number }, neededRessources: translatedRessource[]) => {
-  neededRessources.push({
+let writeNeededRessources = (TranslationFiles: any, element: { uniquename: string, count: number } | { uniquename: string, count: number }[], neededRessources: translatedRessource[][]) => {
+  let tempRessources: translatedRessource[] = []
+  if (Array.isArray(element)) {
+    element.forEach((value)=>{
+      tempRessources.push(reformatRessources(value,TranslationFiles)) 
+    })
+  }
+  else{
+    tempRessources.push(reformatRessources(element,TranslationFiles))
+  }
+  neededRessources.push(tempRessources)
+}
+
+
+let reformatRessources = (element: { uniquename: string, count: number }, TranslationFiles: any) => {
+  return {
     name: element.uniquename, count: element.count, translatedName: TranslationFiles.find((element2: any) => {
       if (element2.LocalizationNameVariable == "ITEMS_" + element.uniquename) {
         return true
       }
       else return false
     }).LocalizedNames.ENUS
-  })
+  }
 }
--- src/TS/structs/item.ts
+++ src/TS/structs/item.ts
@@ -1,4 +1,5 @@
 import { craftingrequirement } from "./craftingrequirement";
+import { craftResource } from "./craftResource";
 import { translatedRessource } from "./translatedRessource";
 
 
@@ -12,7 +13,7 @@ export interface item {
       craftingrequirements: {
         silver: number;
         craftingfocus: number;
-        craftressource: translatedRessource[];
+        craftresource: craftResource[];
       };
     }[];
   };
